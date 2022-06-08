f3f14155db2ac2bdd80d38617550f5b1b4f2e83b
gabriel ferreira <gabriel.nfne@gmail.com>
2022-06-08 12:26:07 -0300

    adicionei 2 rows para o model events

---
+++ db/migrate/20220608145705_add_coordinates_to.rb
@@ -0,0 +1,4 @@
+class AddCoordinatesTo < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_08_003515) do
+ActiveRecord::Schema.define(version: 2022_06_08_145705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2022_06_08_003515) do
   end
 
   create_table "check_ins", force: :cascade do |t|
-    t.boolean "confirmed"
+    t.boolean "confirmed", default: false
     t.bigint "user_id", null: false
     t.bigint "event_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -80,17 +80,6 @@ ActiveRecord::Schema.define(version: 2022_06_08_003515) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["followed_id"], name: "index_follows_on_followed_id"
     t.index ["user_id"], name: "index_follows_on_user_id"
-  end
-
-  create_table "friendships", id: :serial, force: :cascade do |t|
-    t.string "friendable_type"
-    t.integer "friendable_id"
-    t.integer "friend_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "blocker_id"
-    t.integer "status"
-    t.index ["friendable_id", "friend_id"], name: "index_friendships_on_friendable_id_and_friend_id", unique: true
   end
 
   create_table "posts", force: :cascade do |t|
