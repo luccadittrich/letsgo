d76e6773556205418a88a43d8a4135e272e97ee8
Icaro Leon <icvieiramg@gmail.com>
2022-06-09 16:54:40 -0300

    Posts controller ok

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
     root to: "devise/sessions#new"
   end
 
-  resources :events
+  resources :events do
+    resources :posts
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
