59bfaf60434e5e07035890d699dac82f47f5f178
gabriel ferreira <gabriel.nfne@gmail.com>
2022-06-15 16:31:29 -0300

    mapa streets-v11

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/dk",
+      style: "mapbox://styles/mapbox/streets-v11",
       zoom: 16
     })
     this.#addMarkersToMap()
