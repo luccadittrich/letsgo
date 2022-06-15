6753b177cafd6da4ca4d1127b200a9eb5d9b2376
gessica da silva <gessicahugendobler@gmail.com>
2022-06-15 11:46:14 -0300

    inicio do css

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,17 @@
 class ProfilesController < ApplicationController
   def show
+    @feed_header = 'profile'
+    @widget = 'profile'
+    @check_ins = CheckIn.where(user_id: current_user)
     @user = User.find(params[:id])
     @follows = Follow.find_by(user_id: current_user.id, followed_id: params[:id])
+    @events = Event.all
+    @markers = @events.geocoded.map do |event|
+      {
+        lat: event.latitude,
+        lng: event.longitude,
+        image_url: helpers.asset_url("house.png")
+      }
+    end
   end
 end
