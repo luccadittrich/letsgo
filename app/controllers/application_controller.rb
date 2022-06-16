551458d7b1414e5122b76a276fe1019a0da3b7d8
gessica da silva <gessicahugendobler@gmail.com>
2022-06-16 16:36:32 -0300

    pode adicionar banner no profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :bio, :photo, :username])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :bio, :photo, :username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :bio, :photo, :banner, :username])
   end
 
   def after_sign_in_path_for(_resource_or_scope)
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :events, through: :check_ins
   has_many :notifications
   has_one_attached :photo
+  has_one_attached :banner
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
